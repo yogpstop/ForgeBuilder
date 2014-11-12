27c12aef573210404ca8bdaffbb96206321cce68
yogpstop <git@yogpc.com>
2014-11-12 23:02:46 +0900

    Fix json loading

--- src/main/java/com/yogpc/fb/ml/CompleteMinecraftVersion.java
+++ src/main/java/com/yogpc/fb/ml/CompleteMinecraftVersion.java
@@ -1,25 +1,23 @@
 package com.yogpc.fb.ml;
 
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
-
-import com.yogpc.fb.sa.Utils;
+import java.util.Set;
 
 public class CompleteMinecraftVersion {
-  private final Map<Library, Object> libraries = new HashMap<Library, Object>();
+  private final Set<Library> libraries = new HashSet<Library>();
 
   public List<String> getNames() {
     final List<String> ret = new ArrayList<String>();
-    for (final Library l : this.libraries.keySet())
+    for (final Library l : this.libraries)
       if (l != null && l.name != null)
         ret.add(l.name);
     return ret;
   }
 
   public boolean contains(final String s) {
-    for (final Library l : this.libraries.keySet())
+    for (final Library l : this.libraries)
       if (l != null && l.name != null && l.name.startsWith(s))
         return true;
     return false;
@@ -27,6 +25,6 @@ public class CompleteMinecraftVersion {
 
   public void add(final String name) {
     if (name != null)
-      this.libraries.put(new Library(name), Utils.DUMMY_OBJECT);
+      this.libraries.add(new Library(name));
   }
 }
