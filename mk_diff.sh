cfbdcdfb4337b182576e503806f54390c7179b0e
yogpstop <git@yogpc.com>
2015-03-26 02:20:55 +0000

    unique diff file order with msys2 [skip ci]

--- mk_diff.sh
+++ mk_diff.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -eu
-diff -rNdu --strip-trailing-cr *-"$1/java" "$1/java" |
-	grep -Pv "^diff -rNdu --strip-trailing-cr" |
+diff -rNdu --strip-trailing-cr --no-ignore-file-name-case *-"$1/java" "$1/java" |
+	grep -Pv "^[^@\-\+ ]" |
 	perl -pe "s~^---[ \t\f]+.*-$1/java(.+?)\s+\d{4}-\d\d-\d\d\s+\d\d:\d\d:\d\d\.\d+\s+(-|\+)\d{4}[ \t\f]*$~--- a\1~g" |
 	perl -pe "s~^\+\+\+[ \t\f]+$1/java(.+?)\s+\d{4}-\d\d-\d\d\s+\d\d:\d\d:\d\d\.\d+\s+(-|\+)\d{4}[ \t\f]*$~+++ b\1~g" >"patch/$1.patch.new"
